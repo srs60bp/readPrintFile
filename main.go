6487c2dc7ffb4588c71c0a6a7fc2d30309aa440d
srs60 <srs60@abv.bg>
2017-11-27 22:53:46 +0200

    read file

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-	//	for i := range os.Args {
-	//		fmt.Println("arg: ", i, os.Args[i])
-	//	}
-
 	file, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -22,6 +18,4 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(string(data[:count]))
-	//	io.Copy(os.Stdout, )
-
 }
