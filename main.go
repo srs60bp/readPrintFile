3a6ad9a7f6f719912fdd8b6c12894dced598c707
srs60 <srs60@abv.bg>
2017-11-27 22:51:54 +0200

    read file

--- main.go
+++ main.go
@@ -6,7 +6,22 @@ import (
 )
 
 func main() {
-	for i := range os.Args {
-		fmt.Println("arg: ", i, os.Args[i])
+	//	for i := range os.Args {
+	//		fmt.Println("arg: ", i, os.Args[i])
+	//	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
 	}
+	data := make([]byte, 32*1024)
+	count, errF := file.Read(data)
+	if errF != nil {
+		fmt.Println("Error: ", errF)
+		os.Exit(1)
+	}
+	fmt.Println(string(data[:count]))
+	//	io.Copy(os.Stdout, )
+
 }
